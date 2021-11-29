33a25137427004d78d60f70041950eac6ac42624
Lasha <50033103+MistbornL@users.noreply.github.com>
2021-11-29 23:54:15 +0400

    hou yeah

--- dragon-tiger-frontend/src/Home.js
+++ dragon-tiger-frontend/src/Home.js
@@ -1,42 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const Home = () => {
-    const [games, setGames] = useState([])
+  const [games, setGames] = useState([]);
 
-    const getGames = async () => {
-        const response = await axios.get("http://localhost:8000/api/get/all/game")
-        setGames(response.data)
-    }
+  // getGames agzavnis requests chvenst apistan romlis misamartia api/get/all/game da mogvaqvs magidis shesaxeb informacia rasac vinaxavt setGames it
+  const getGames = async () => {
+    const response = await axios.get("http://localhost:8000/api/get/all/game");
+    setGames(response.data);
+  };
 
+  // es renderi moxdeba tuara srazu simon agzavnis requests anu zemota logikas simon
+  useEffect(() => {
+    getGames();
+  }, []);
 
-    useEffect(() => {
-        getGames()
-    }, [])
-
-
-    const renderGames = () => {
-        return games.map(game => {
-            return (
-                <div>
-                    <h1>{game.name}</h1>
-                    <h2>{game._id}</h2>
-                    <a href={`http://localhost:3000/game/${game._id}`}>GADAVEDIT </a>
-                </div>
-            )
-        })
-    }
-
-    return (
+  const renderGames = () => {
+    // roca game is data davimaxsovret eg iyo listi es warmoidginet rogorc for cikli da amogvaq tito mokled rac shedegad gvadzlevs magidis saxels da id
+    return games.map((game) => {
+      return (
         <div>
-            <h1>
-                Home
-                {renderGames()}
-            </h1>
+          <h1>{game.name}</h1>
+          <h2>{game._id}</h2>
+          {/* es <a> tag gvadzlevs linkad gadaqcevis shesadzlebloas da mag linkshi mivecit game id ar deibnet exla simon sheberet  */}
+          <a href={`http://localhost:3000/game/${game._id}`}>GADAVEDIT </a>
         </div>
+      );
+    });
+  };
 
-    )
-}
+  // aq ubralod h1 shi varenderebt im informacias rac wamoviget
+  return (
+    <div>
+      <h1>
+        Home
+        {renderGames()}
+      </h1>
+    </div>
+  );
+};
 
+// vso magia dalshe uberet kitxvebis shemtxvevashi momweret imedia gipasuxebt mara didi imedi mainc nu geqnebat mainc ravici :DDDDDDDDDD he shecxet axla
 export default Home;
